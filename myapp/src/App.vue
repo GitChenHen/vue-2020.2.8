620718da336a679dc8ddb947e4256b0a808112a0
GitChenHen <chd_ahen@126.com>
2020-02-08 18:22:10 +0800

    第二次

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Test msg="测试测试"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Test
   }
 }
 </script>
---
+++ myapp/src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="test">
+        <h3>{{msg}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: "test",
+    props: {
+        msg: String
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
